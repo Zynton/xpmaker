01ad74e054118f2a0e7fdd8e4ba4bd0ad52435cf
Antoine Guenet <aguenet@gmail.com>
2017-11-07 12:53:06 +0100

    Remove submit and unnecessary python imports.

--- manage.py
+++ manage.py
@@ -10,13 +10,6 @@ from flask import (Flask,
                    send_from_directory)
 from flask_bower import Bower
 
-import re
-
-from xpmaker import (parse_input,
-                     create_vexflow_xp,
-                     create_abcjs_xp,
-                     xp_mix_and_match)
-
 app = Flask(__name__, static_url_path='/static')
 app.debug = True
 Bower(app)
@@ -25,25 +18,6 @@ Bower(app)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def submit():
-    title = request.form['title'] or 'XP'
-
-    bpm = request.form['bpm'] or 100
-
-    notes_str = request.form['notes']
-    notes = parse_input(notes_str)
-
-    rhythms_str = request.form['rhythms']
-    rhythms_str = re.sub("\D", " ", rhythms_str)
-    rhythms = rhythms_str.split()
-    rhythms = rhythms[1:]
-
-    xp = xp_mix_and_match(notes, rhythms)
-    xp_abcjs, ts = create_abcjs_xp(xp)
-
-    return render_template('abcjs.html', xp=xp_abcjs, ts=ts, title=title, bpm=bpm)
 
 @app.route('/soundfont/<path:path>')
 def send_js(path):
