c9447f383b3a03774de46544565d79cef1dd1ed3
Antoine Guenet <aguenet@gmail.com>
2017-11-07 13:36:22 +0100

    Add realtime update.

--- static/js/xpmaker.js
+++ static/js/xpmaker.js
@@ -14,8 +14,25 @@ function updateXP() {
 	var xp = xp_mix_and_match(notes, rhythms);
 	var xp_abcjs = create_abcjs_xp(xp) || "";
 
+	// Update XP Input
 	abcjs_text = mk_abcjs_text(title, bpm, xp_abcjs);
-	update_text_field('xp_input', abcjs_text);
+	xp_input = $('#xp_input');
+	xp_input.val(abcjs_text);
+
+	//simulate(document.getElementById('xp_input'), "focus");
+	//xp_editor.renderTune(xp_editor, {}, xp_canvas);
+	xp_editor = new ABCJS.Editor("xp_input", { canvas_id: "xp_canvas",
+												 		 midi_id: "xp_midi",
+												 		 midi_download_id: "xp_midi_dl",
+												 		 midi_options: {
+												 		 	generateDownload:"true",
+												 		 	downloadLabel:"Download %T.mid"
+												 		 },
+												 		 warnings_id: "warnings",
+												 		 parser_params: {},
+												 		 generate_midi: true,
+												 		 generate_warnings: false
+											    });
 };
 
 function update_text_field(field_id, text) {
@@ -81,3 +98,69 @@ function parse_rhythm(rhythm_str) {
 function note_rhythm_to_abcjs(note_str, rhythm_str) {
 	return note_str + '/' + rhythm_str
 };
+
+
+
+
+
+// Simulate event
+function simulate(element, eventName)
+{
+    var options = extend(defaultOptions, arguments[2] || {});
+    var oEvent, eventType = null;
+
+    for (var name in eventMatchers)
+    {
+        if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+    }
+
+    if (!eventType)
+        throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+    if (document.createEvent)
+    {
+        oEvent = document.createEvent(eventType);
+        if (eventType == 'HTMLEvents')
+        {
+            oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+        }
+        else
+        {
+            oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+            options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+            options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+        }
+        element.dispatchEvent(oEvent);
+    }
+    else
+    {
+        options.clientX = options.pointerX;
+        options.clientY = options.pointerY;
+        var evt = document.createEventObject();
+        oEvent = extend(evt, options);
+        element.fireEvent('on' + eventName, oEvent);
+    }
+    return element;
+}
+
+function extend(destination, source) {
+    for (var property in source)
+      destination[property] = source[property];
+    return destination;
+}
+
+var eventMatchers = {
+    'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+    'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+}
+var defaultOptions = {
+    pointerX: 0,
+    pointerY: 0,
+    button: 0,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    bubbles: true,
+    cancelable: true
+}
