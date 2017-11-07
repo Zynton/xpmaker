edea20b5b9c91300c7965713b80c73dae61faadd
Antoine Guenet <aguenet@gmail.com>
2017-11-07 12:51:05 +0100

    Translate all relevant Python code to Javascript: abcjs xp generated.

--- static/js/xpmaker.js
+++ static/js/xpmaker.js
@@ -1,6 +1,6 @@
 function updateXP() {
-	var title = $('#title_input').val();
-	var bpm = $('#bpm_input').val();
+	var title = $('#title_input').val() || "XP";
+	var bpm = $('#bpm_input').val() || "100";
 
 	var notes_str = $('#notes_input').val();
 	var notes = notes_str.split(/[ ]+/);
@@ -11,11 +11,24 @@ function updateXP() {
 	rhythms.splice(0, 2);
 
 	var xp = xp_mix_and_match(notes, rhythms);
-	console.log(xp);
-	//var xp_abcjs, ts = create_abcjs_xp(xp);
+	var xp_abcjs = create_abcjs_xp(xp) || "";
 
-	//var xp_input = $('#xp_input');
-	//xp_input.val(xp_abcjs);
+	abcjs_text = mk_abcjs_text(title, bpm, xp_abcjs);
+	update_text_field('xp_input', abcjs_text);
+};
+
+function update_text_field(field_id, text) {
+	$('#' + field_id).val(text);
+};
+
+function mk_abcjs_text(title, bpm, xp_abcjs) {
+	var abcjs_text = "T: " + title;
+	abcjs_text += "\nL: 1";
+	abcjs_text += "\nQ: " + bpm;
+	//abcjs_text += "\nM: " + ts[0] + "/" + ts[1];
+	abcjs_text += "\n||:" + xp_abcjs + ":||";
+
+	return abcjs_text;
 };
 
 function xp_mix_and_match(notes, rhythms) {
@@ -33,4 +46,38 @@ function xp_mix_and_match(notes, rhythms) {
 		xp.push(note);
 	};
 	return xp;
+};
+
+function create_abcjs_xp(xp) {
+	var length = 0;
+	var abcjs_str = "";
+	console.log(xp);
+	for (var i = 0; i < xp.length; i++) {
+		evt = xp[i];
+		length += parse_rhythm(evt[1]);
+		note_rhythm = note_rhythm_to_abcjs(evt[0], evt[1]);
+
+		abcjs_str += note_rhythm + " ";
+	};
+
+	//var time_signature = length_to_time_signature(length);
+	return abcjs_str;//, time_signature;
+};
+
+function parse_rhythm(rhythm_str) {
+	tuplet_re = /.*:(\d+)$/;
+	
+	if (rhythm_str[rhythm_str.length - 1] == '.') {
+		var rhythm_value = parseInt(rhythm_str);
+		var rhythm_float = rhythm_value * 2/3;
+	} else {
+		rhythm_float = parseFloat(rhythm_str);
+	};
+	
+	var length_in_beats = 4 / rhythm_float;
+	return length_in_beats;
+};
+
+function note_rhythm_to_abcjs(note_str, rhythm_str) {
+	return note_str + '/' + rhythm_str
 };
